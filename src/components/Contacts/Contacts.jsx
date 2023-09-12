185d561f08bb3c6dcccaa39e00adda5931e00476
Nataliia <natalabojko@gmail.com>
2023-09-12 15:30:26 +0300

    started Hero

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Contacts.module.scss";
+const Contacts = () => {
+  return <section className={styles.section}>Contacts</section>;
+};
+
+export default Contacts;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Hero.module.scss";
+const Hero = () => {
+  return <section className={styles.section}>Hero</section>;
+};
+
+export default Hero;

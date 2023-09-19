7df5f8a886e749e47cc6ebf3a0d24d0aeb531852
Nataliia <natalabojko@gmail.com>
2023-09-19 23:05:55 +0300

    backgrounds almost done...

--- src/sections/Contacts/Contacts.jsx
+++ src/sections/Contacts/Contacts.jsx
@@ -7,16 +7,6 @@ import TelEmail from "@/components/TelEmail/TelEmail";
 const Contacts = () => {
   return (
     <section id="contacts" className={styles.contacts}>
-      {/* <div className={styles.contactsWrapper}>
-        <div className={styles.contact}>
-          <AiFillMail />
-          <a href="tel:+380966058605"> +38 (096) 605 86 05</a>
-        </div>
-        <div className={styles.contact}>
-          <AiFillPhone />
-          <a href="mailto:inbox.webevery@gmail.com">inbox.webevery@gmail.com</a>
-        </div>
-      </div> */}
       <TelEmail />
       <Form />
     </section>

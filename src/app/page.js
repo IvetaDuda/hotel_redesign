391a8683e4d48a30a7cfc4e7e4dd440c05f4d68e
Nataliia <natalabojko@gmail.com>
2023-09-19 15:14:01 +0300

    page img

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,15 @@ import Contacts from "@/sections/Contacts/Contacts";
 import Footer from "@/components/Footer/Footer";
 import Hero from "@/sections/Hero/Hero";
 import styles from "./page.module.scss";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      <div className={styles.imgContainer}>
+        <Image src="/HeroFigmaS.webp" fill={true} />
+      </div>
+
       <Hero />
       <About />
       <div className={styles.wrapper}>

851101fa00d1fba3fda6ed55f3a0587d7324b8d7
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2023-11-15 17:20:24 +0200

    add photo

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,17 @@
-import About from '@/sections/About/About';
+// import About from '@/sections/About/About';
 
 import Hero from '@/sections/Hero/Hero';
 import styles from './page.module.scss';
-import Image from 'next/image';
-import BottomSection from '@/sections/BottomSection/BottomSection';
-import TelEmail from '@/components/TelEmail/TelEmail';
 import ContactLink from '@/components/ContactLink/ContatLink';
+import AboutUs from '@/sections/AboutUs/AboutUs';
 
 export default function Home() {
   return (
-    // <>
-    <div className={styles.heroWrapper}>
-      {/* <div className={styles.imgContainer}>
-          <Image
-            src="/jonathan-ansel-moy-de-vitry-Fvb8EH50PxQ-unsplash.jpg"
-            fill={true}
-            alt="nature"
-          />
-        </div> */}
-
+    <>
       <Hero />
-      {/* </div> */}
       <ContactLink />
-      <About />
-      {/* <BottomSection /> */}
-    </div>
-    // </>
+      <AboutUs />
+      {/* <About /> */}
+    </>
   );
 }
---
+++ src/sections/AboutUs/AboutUs.jsx
@@ -0,0 +1,7 @@
+import styles from './AboutUs.module.scss';
+
+const AboutUs = () => {
+  return <section className={styles.container}></section>;
+};
+
+export default AboutUs;

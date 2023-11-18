44fabed00fa01f5d13152cd103b8447616e41fd1
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2023-11-18 18:04:58 +0200

    add about us

--- src/sections/AboutUs/AboutUs.jsx
+++ src/sections/AboutUs/AboutUs.jsx
@@ -1,7 +1,23 @@
 import styles from './AboutUs.module.scss';
 
 const AboutUs = () => {
-  return <section className={styles.container}></section>;
+  return (
+    <section className={styles.container}>
+      <ul className={styles.aboutContainer}>
+        <li className={styles.aboutItem}>
+          <div className={styles.aboutContent}>
+            <h3 className={styles.aboutTitle}>Котеджі</h3>
+            <p className={styles.aboutText}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              necessitatibus a vero odit quod minima atque nemo porro rerum
+              culpa dolores asperiores quasi saepe, aliquam suscipit,
+              consequatur, quisquam veniam ratione!
+            </p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default AboutUs;

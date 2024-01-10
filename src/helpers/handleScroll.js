9f127f489c6651ed278c362ff721519f2969df90
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-01-10 18:11:09 +0200

    fix scroll

--- src/helpers/handleScroll.js
+++ src/helpers/handleScroll.js
@@ -1,27 +1,24 @@
 export const handleScroll = (e) => {
-  // перевіряємо, чи це дійсно DOM-елемент
-  if (!(e.target instanceof Element)) {
-    return;
-  }
+  if (typeof window !== 'undefined') {
+    const href = e.target.getAttribute('href');
 
-  // отримуємо href з елемента, який має викликав подію
-  const href = e.target.getAttribute('href');
+    if (!href) {
+      return;
+    }
 
-  if (!href) {
-    return;
-  }
+    const targetId = href.replace(/.*\#/, '');
+    const elem = document.getElementById(targetId);
 
-  const targetId = href.replace(/.*\#/, '');
-  const elem = document.getElementById(targetId);
-
-  if (elem) {
-    e.preventDefault();
-    elem.scrollIntoView({
-      behavior: 'smooth',
-    });
+    if (elem) {
+      e.preventDefault();
+      elem.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
   }
 };
 
-// Додаємо прослушувач подій з passive флагом
-
-document.addEventListener('scroll', handleScroll, { passive: true });
+// Додаємо прослушувач подій з passive флагом, якщо викликається в браузері
+if (typeof window !== 'undefined') {
+  document.addEventListener('scroll', handleScroll, { passive: true });
+}

61174ca72865b50db107280dec7ed3ebbcbfa40f
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-01-11 17:11:15 +0200

    hero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,17 +2,27 @@
 
 import { handleScroll } from '@/helpers/handleScroll';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Hero.module.scss';
 const Hero = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
   return (
     <section className={styles.hero} id="hero">
-      <h1 className={styles.heroTitle}>MAGIC PLACE</h1>
-      <p className={styles.description}>
-        Дозвольте собі відпочинок у спокої та гармонії серед захоплюючого
-        гірського пейзажу, де кожна відпустка - це незабутня подорожжю до
-        природи.
-      </p>
+      {!isLoading && (
+        <>
+          <h1 className={styles.heroTitle}>MAGIC PLACE</h1>
+          <p className={styles.description}>
+            Дозвольте собі відпочинок у спокої та гармонії серед захоплюючого
+            гірського пейзажу, де кожна відпустка - це незабутня подорожжю до
+            природи.
+          </p>
+        </>
+      )}
       <Link className={styles.heroArrow} href="#about" onClick={handleScroll}>
         <svg className={styles.heroArrowSvg}>
           <use href="sprite.svg#icon-chevron-down" />

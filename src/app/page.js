c3258f825164e77db5b3998da18db0c7590f926e
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2024-01-12 16:09:00 +0200

    add validatinon to form

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ import dynamic from 'next/dynamic';
 const DynamicAboutUs = dynamic(() => import('@/sections/AboutUs/AboutUs'));
 const DynamicContacts = dynamic(() => import('@/sections/Contacts/Contacts'));
 const DynamicServices = dynamic(() => import('@/sections/Services/Services'));
-const DynamicForm = dynamic(() => import('@/sections/Form/Form'));
+const DynamicForm = dynamic(() => import('@/sections/Form/FormHook'));
+// const DynamicForm = dynamic(() => import('@/sections/Form/Form'));
 
 export default function Home() {
   return (
---
+++ src/helpers/regularExspression.js
@@ -0,0 +1,1 @@
+export const regexPhone = /^\+\d{12}$/;

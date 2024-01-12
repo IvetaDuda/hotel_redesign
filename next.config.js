615b500189bc32a21d2af5c2f74ab69dd443a381
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-01-12 07:48:47 +0200

    fix code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2592000, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+};

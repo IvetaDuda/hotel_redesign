6e93cb397653c9ec435499f3450223cb1a1b81ec
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-01-12 10:41:48 +0200

    test

--- src/helpers/handleScroll.js
+++ src/helpers/handleScroll.js
@@ -1,4 +1,8 @@
+let isScrolling = false;
+
 export const handleScroll = (e) => {
+  if (isScrolling) return;
+
   if (typeof window !== 'undefined' && e.target instanceof Element) {
     const href = e.target.getAttribute('href');
 
@@ -11,14 +15,45 @@ export const handleScroll = (e) => {
 
     if (elem) {
       e.preventDefault();
+      isScrolling = true;
       elem.scrollIntoView({
         behavior: 'smooth',
       });
+
+      setTimeout(() => {
+        isScrolling = false;
+      }, 1000); // Додайте час затримки у мілісекундах
+
+      document.removeEventListener('scroll', handleScroll);
+
+      setTimeout(() => {
+        document.addEventListener('scroll', handleScroll, { passive: true });
+      }, 1000); // Додайте час затримки у мілісекундах
     }
   }
 };
 
-// Додаємо прослушувач подій з passive флагом, якщо викликається в браузері
-if (typeof window !== 'undefined') {
-  document.addEventListener('scroll', handleScroll, { passive: true });
-}
+// export const handleScroll = (e) => {
+//   if (typeof window !== 'undefined' && e.target instanceof Element) {
+//     const href = e.target.getAttribute('href');
+
+//     if (!href) {
+//       return;
+//     }
+
+//     const targetId = href.replace(/.*\#/, '');
+//     const elem = document.getElementById(targetId);
+
+//     if (elem) {
+//       e.preventDefault();
+//       elem.scrollIntoView({
+//         behavior: 'smooth',
+//       });
+//     }
+//   }
+// };
+
+// // Додаємо прослушувач подій з passive флагом, якщо викликається в браузері
+// if (typeof window !== 'undefined') {
+//   document.addEventListener('scroll', handleScroll, { passive: true });
+// }

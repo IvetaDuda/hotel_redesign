f5236f6bc982e658c1f3d752c58daf4d3877d44e
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-01-13 12:21:27 +0200

    test

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Hero from '@/sections/Hero/Hero';
 import dynamic from 'next/dynamic';
 // import styles from './page.module.scss';
 
-const DynamicHero = dynamic(() => import('@/sections/Hero/Hero'));
 const DynamicAboutUs = dynamic(() => import('@/sections/AboutUs/AboutUs'));
 const DynamicContacts = dynamic(() => import('@/sections/Contacts/Contacts'));
 const DynamicServices = dynamic(() => import('@/sections/Services/Services'));
@@ -11,7 +10,7 @@ const DynamicForm = dynamic(() => import('@/sections/Form/FormHook'));
 export default function Home() {
   return (
     <>
-      <DynamicHero />
+      <Hero />
       <DynamicAboutUs />
       <DynamicServices />
       <DynamicForm />
